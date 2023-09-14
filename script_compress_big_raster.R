5092717e0acf6dd0d30ca91cfea64b91f520ad38
Renata Muylaert <renatamuy@gmail.com>
2023-09-14 13:47:24 +1200

    compress

--- script_compress_big_raster.R
+++ script_compress_big_raster.R
@@ -1,13 +1,84 @@
+# Compressing rasters
+# Rê 2023 
+# R 4.3.1
 
+# Packages 
 
+require(terra)
+require(rgrass)
 
+setwd('F://') 
 
 # Open
 
-# Export compressed
+grassDir='C:/Program Files/GRASS GIS 8.2'
 
-# Export further compressed with float 32
+im="serang_mercator.tif"
 
-# Export transformed to less decs
+rgrass::initGRASS(gisBase = grassDir,
+                  SG = rast(im),
+                  gisDbase = "grassdb",
+                  location = "default",  
+                  mapset = "PERMANENT",
+                  override = TRUE, 
+                  remove_GISRC = TRUE)
+
+rgrass::execGRASS("r.in.gdal",
+                  input = im,
+                  output = "rast",
+                  flags = c("overwrite"))
+
+rgrass::execGRASS("g.region",
+                  raster="rast")
+
+# Export not compressed float 64
+
+rgrass::execGRASS("r.out.gdal",
+                  input="rast",
+                  output="serang_compressed_f64nc.tif",
+                  format="GTiff",
+                  type="Float64",
+                  flags="overwrite")
+
+# Export compressed float 64
+
+rgrass::execGRASS("r.out.gdal",
+                  input="rast",
+                  output="serang_compressed_f64.tif",
+                  format="GTiff",
+                  type="Float64",
+                  flags="overwrite",
+                  createopt="TFW=YES,COMPRESS=DEFLATE,BIGTIFF=YES")
+
+# Export compressed byte
+
+rgrass::execGRASS("r.out.gdal",
+                  input="rast",
+                  output="serang_compressed_byte.tif",
+                  format="GTiff",
+                  type="Float32",
+                  flags="overwrite",
+                  createopt="TFW=YES,COMPRESS=DEFLATE,BIGTIFF=YES")
+
+# Export further compressed float 32
+
+rgrass::execGRASS("r.out.gdal",
+                  input="rast",
+                  output="serang_compressed_f32.tif",
+                  format="GTiff",
+                  type="Float32",
+                  flags="overwrite",
+                  createopt="TFW=YES,COMPRESS=DEFLATE,BIGTIFF=YES")
+
+# Export not compressed float 32
+
+rgrass::execGRASS("r.out.gdal",
+                  input="rast",
+                  output="serang_compressed_f32nc.tif",
+                  format="GTiff",
+                  type="Float32",
+                  flags="overwrite")
+
+#-----------------------------
 
 
