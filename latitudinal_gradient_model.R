a9cdbd639b691efb3c4552cf7b5eccf0a10e24e6
Renata Muylaert <renatamuy@gmail.com>
2023-09-20 13:08:40 +1200

    latitude correction

--- latitudinal_gradient_model.R
+++ latitudinal_gradient_model.R
@@ -4,12 +4,14 @@ latitudinal_model <- function(latitude, peak, k) {
   return(peak * exp(-k * abs(latitude)))
 }
 
-latitude <- 30  # Example latitude (adjust as needed)
-
-peak <- 532 + 191  # Peak biodiversity or prior (such as maximum limit from a raster?)
+latitude <- 30  # Remember we have projected data!
 
 # Max limit for mammals in 1 sqkm = 191
 # Max limit for birds in 1sqkm = 532
+
+
+peak <- 532 + 191 
+
 
 k <- 0.02      
 
@@ -48,3 +50,21 @@ plot(latitudes, biodiversity, type = "l",
      main = "Latitudinal Biodiversity Pattern (Gaussian Decay)")
 
 #----------------------------------------------------
+
+# Informations about our projected data:
+
+# EPSG:3857
+
+origin=c(0,0)
+
+Bounding_box = c(
+  minlong = -180,
+  minlat= -85.06,
+  maxlong=180,   
+  maxlat=85.06 )
+
+Projected bounds=  c( 
+    minlong= -20037508.34,
+    minlat= -20048966.1,
+    maxlong=20037508.34, 
+    maxlat=20048966.1)
