2841c8842162beece2e7c700c8015a61f8a93680
Renata Muylaert <renatamuy@gmail.com>
2024-12-12 10:56:03 +1300

    figs

--- script/02_IUCN_Ranges_Virion_Figure_S2.R
+++ script/02_IUCN_Ranges_Virion_Figure_S2.R
@@ -1,7 +1,7 @@
-# Simple plot viruses ~ mammal hosts
-# Virus layer - detailed resolution
-# Viruses from mammals 
-# Mammal hosts layer IUCN 2021
+# Plot viruses ~ mammal hosts
+# Virus layer source: Virion
+# Source: Mammal hosts layer IUCN 2021
+# Viruses from mammals filter
 
 require(raster)
 require(here)
@@ -12,39 +12,41 @@ require(fasterize)
 
 setwd("D://OneDrive - Massey University//hostland//virion-main/Virion")
 
+# open data
 virion <- vroom("Virion.csv.gz")
 
+# get a base raster
 rbase <- raster('D://OneDrive - Massey University//_env//_env_27km_ss6//bio_1.tif')
 
+# blank it for the world
 r <- raster::raster(ext=extent(c(-180, 180, -90, 90)), crs = crs(rbase), res=res(rbase))
 
+# get mammals
 iucn <- st_read(dsn = 'D:/OneDrive - Massey University/_env/mammals/IUCN_2021',
                 layer = 'MAMMALS_TERRESTRIAL_ONLY')
 
+# get mammal viruses 
 virion %>% filter(HostClass == "mammalia") %>%
   dplyr::select(Host, Virus) %>%
   distinct() %>%
   group_by(Host) %>%
   summarize(NVirus = n_distinct(Virus)) -> nvir
 
-
+# get mammal viruses and join with mammals
 iucn %>% mutate(binomial = tolower(binomial)) %>%
   left_join(nvir, by = c('binomial' = 'Host')) %>%
   filter(NVirus > 0) -> iucn
 
-# Number of hosts
-
+# number of mammal hosts
 map.num.m <- fasterize(iucn, r, field = NULL, fun = 'count')
-map.num.m
 
-# Number of viruses
-
+# number of mammal viruses
 map.sum.m <- fasterize(iucn, r, field = "NVirus", fun = 'sum')
 
-map.sum.m
-
+# model
 modelhv <- glm(values(map.sum.m)~ values(map.num.m))
 
+# export
 setwd('C:/Users/rdelaram/Documents/GitHub/eride/results/')
 
 jpeg("Figure_S2.jpg", width = 18, height = 18, units='cm', res = 300)
@@ -57,7 +59,6 @@ n_distinct(iucn$binomial) / n_distinct(nvir$Host)
 
 hosts <- raster::stack(map.num.m, map.sum.m )
 
-
 #------------
 setwd(here())
 
@@ -68,7 +69,7 @@ setwd('pull_virus_IUCN_2021')
 
 names(hosts) <- c("mammal_hosts", 'mammal_viruses')
 
-
+# export rasters
 #setwd('F://')
 setwd(here())
 setwd('results')
@@ -79,8 +80,8 @@ terra::writeRaster(hosts$mammal_hosts, file = paste(names(hosts)[1], "_27km.tif"
 
 
 raster::writeRaster(hosts$mammal_viruses, file = paste(names(hosts)[2], "_27km.tif", sep=""), 
-                   format="GTiff", 
-                  bylayer=TRUE,
-              overwrite=TRUE)
+                    format="GTiff", 
+                    bylayer=TRUE,
+                    overwrite=TRUE)
 
-#------
+#-------------------------------------------------------------------------------------------
