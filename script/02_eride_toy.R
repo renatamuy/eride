e4f1d46b933f2680c01511e976fca73b68f73a7d
Renata Muylaert <renatamuy@gmail.com>
2025-07-01 09:41:56 +1000

    updates

--- script/02_eride_toy.R
+++ script/02_eride_toy.R
@@ -2,8 +2,9 @@
 # R 4.4.1 Race for Your Life        
 # eRIDE - March 2025
 # Toy data: East of Nagreg (around -6.38 latitude and 106.56 longitude)
-# Packages ---------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------
 
+# Packages ---------------------------------------------------------------------------------------------------
 require(terra)
 require(rgrass)
 require(here)
@@ -15,8 +16,8 @@ setwd(here())
 setwd('toy_data')
 
 # Find grass
-#Linux
-#grassDir='/opt/nesi/CS400_centos7_bdw/GRASS/8.2.1-gimkl-2022a/grass82'
+# Linux
+# grassDir='/opt/nesi/CS400_centos7_bdw/GRASS/8.2.1-gimkl-2022a/grass82'
 
 #Windows
 grassDir='C:/Program Files/GRASS GIS 8.2'
@@ -40,7 +41,6 @@ rgrass::execGRASS("r.in.gdal",
                   flags = c("overwrite"))
 
 # Importing pop
-
 impop="pop.tif"
 
 # Creating rast 
@@ -66,15 +66,13 @@ values(blank_raster) <- NA
 
 writeRaster(blank_raster, filename = 'blank_raster.tif', overwrite=TRUE)
 
-# Set the extent of the environment to that of you image ----------------
-
+# Set the extent of the environment to that of you image ------------------------
 rgrass::execGRASS("r.in.gdal",
                   input = 'blank_raster.tif',
                   output = "blank_raster",
                   flags = c("overwrite"))
 
 # Set working resolution 
-
 wres <- '30' # 3 arc (~100 m)
 
 rgrass::execGRASS("g.region",
@@ -86,14 +84,13 @@ rgrass::execGRASS("g.list", type = "raster")
 
 # ERIDE ---------------
 # The larger the radius, the slower the processing time
-# default function radius is 10, we keep it as 10 as it approximates to 20 x 20 window size from Wilkinson et al (2018)
+# default function radius is 10, we keep it as 10 as it approximates to 20 x 20 window size from Wilkinson et al. (2018)
 # default nproc=7, default memory = 1000
 # Run eride and PAR with specified parameters (all default but z, in this case)
 
 eride_run("rast", "pop", z = 0.20)
 
 # Get results
-
 results <- rast(imr)
 results$fragments=rast("fragments.tif")
 results$fragment_area <- rast("areas.tif")  
@@ -103,6 +100,7 @@ results$weighted_boundaries <- rast("wb.tif")
 results$eRIDE<- rast("eRIDE.tif")
 results$PAR <- rast('PAR.tif')
 
+# Export figure
 png(filename='results_toy.jpg', res=300, units = 'cm', width= 18, height = 10)
 plot(results)
 dev.off()
